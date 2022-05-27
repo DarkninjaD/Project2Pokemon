54d6cd7fa35db863af697090dbd283a8743f9e82
Kyle Dilick <kdilick@gmail.com>
2022-05-27 15:01:41 -0600

    implementing data fetch and rough landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { PokeContext } from './Components/PokeContext'
+import LandingPage from './Components/LandingPage'
 import './App.css';
 
+
+//url: https://pokeapi.co/api/v2/pokemon/?limit=151
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState([]);
+
+  const changePokemon = (param) => {
+    setPokemon(param)
+  }
+
+  const value = [pokemon, changePokemon]
+
+  useEffect(() => {
+    fetch ('https://pokeapi.co/api/v2/pokemon/?limit=151')
+      .then(res => res.json())
+      .then(data => data.results)
+      .then(pokemonList => Promise.all(pokemonList.map(pokemon => fetch(pokemon.url).then(response => response.json()))))
+      .then(results => {
+        setPokemon(results);
+        setIsLoading(false);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokeContext.Provider value={value}>
+        <LandingPage isLoading={isLoading}></LandingPage>
+      </PokeContext.Provider>
     </div>
   );
 }
---
+++ src/Components/LandingPage.js
@@ -0,0 +1,12 @@
+
+const LandingPage = ({ isLoading }) => {
+  return (
+    <>
+      <img src=""></img>
+      <p>Pokemon Time</p>
+      {isLoading ? <button>Loading, Please Wait</button> : <button>Enter</button>}
+    </>
+  )
+}
+
+export default LandingPage;
