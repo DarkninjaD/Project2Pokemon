c8e8dd79cd93658be813a0fa6acfe2627f612bd0
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-05-31 14:11:12 -0500

    Layoutttt

--- src/components/ForestRegion/ForestRegion.jsx
+++ src/components/ForestRegion/ForestRegion.jsx
@@ -1,20 +1,35 @@
 import mokePoke from "../../mock/pokeExample";
 import { PokeDexButton } from "../ToolBar/PokeDexButton";
-import ForestBackground from "../../assets/Forest-Background.svg";
 import "./ForestRegion.css";
 import { LocationContainer } from "../LocationContainer/LocationContainer";
+import { useState } from "react";
+import mokeImg from "../../mock/mockImage.png";
 
 const ForestRegion = () => {
+  const makediv = 24;
+  console.log(mokePoke);
+  console.log(mokePoke[0].sprites);
+  console.log(mokePoke[0].sprites.front_default);
   return (
-    <div className="base-grid">
-      <img
-        className="background"
-        src={ForestBackground}
-        alt="background-forest"
-      />
-      <LocationContainer />
-      <PokeDexButton className="pokedex-button" />
-    </div>
+    <body>
+      <div className="row">
+        <div className="base-grid">
+          {[...Array(makediv)].map((elm, index) => {
+            return (
+              <div className="gridContainer">
+                <img src={mokeImg} alt="temp" />
+              </div>
+            );
+          })}
+        </div>
+        <div class="text-center">
+          <PokeDexButton className="pokedex-button" />
+        </div>
+      </div>
+      <div className="locations-box">
+        <LocationContainer />
+      </div>
+    </body>
   );
 };
 export { ForestRegion };
