b325a0f410819580d8f17fd74c4dc844cce41875
Kyle Dilick <kdilick@gmail.com>
2022-06-02 08:35:12 -0600

    pokedex and some css

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { PokemonThumbnail } from "../PokemonThumbnail/PokemonThumbnail";
 import { useNavigate, useLocation } from 'react-router-dom'
 import { PokeContext } from "../PokeContext"
 import "./Pokedex.css";
 
 const Pokedex = ({ pokemonList }) => {
+  const { pokemon, setPokemon } = useContext(PokeContext);
   const [viewPokemon, setViewPokemon] = useState([]);
   const location = useLocation();
   const navigate = useNavigate();
   let lastLocation = location.state.lastLocation;
-  if (lastLocation === undefined) {lastLocation='forest'}
+  if (lastLocation === undefined) { lastLocation = 'forest' }
+
+  const giveMeAllOfThosePokemon = () => {
+    let temp = pokemon
+    temp.forEach(element => {
+      element.isCaught = true;
+    })
+    setPokemon(temp);
+    setViewPokemon();
+  }
+
 
   return (
     <>
@@ -19,18 +30,19 @@ const Pokedex = ({ pokemonList }) => {
         alt={`background-${lastLocation}`}
       />
       <div className="pokedex-container">
-        {/* <button onclick="history.back()">Go back</button> */}
+        <button className="debug" onClick={() => { giveMeAllOfThosePokemon() }}>give me all of those pokemon</button>
+        <button className="back-to-catching-those-pokemon" onClick={() => { navigate(-1) }}>get back to those pokemans</button>
         <h4 className="pokedex-header">Pokedex</h4>
         <div className="pokedex-grid">
           {pokemonList.map((pokemon) => {
             return (
               <PokemonThumbnail
-              key={pokemon.name}
-              pokemon={pokemon}
-              setViewPokemon={setViewPokemon}
+                key={pokemon.name}
+                pokemon={pokemon}
+                setViewPokemon={setViewPokemon}
               />
-              );
-            })};
+            );
+          })};
         </div>
       </div>
     </>
--- src/components/PokemonThumbnail/PokemonThumbnail.js
+++ src/components/PokemonThumbnail/PokemonThumbnail.js
@@ -8,31 +8,99 @@ function capitalize(string) {
 const PokemonThumbnail = ({ pokemon }) => {
 
   return (
-    <div className="pokemon-thumbnail-container">
-      <div className="pokemon-thumbnail">
-        {pokemon.isCaught ?
-        <>
-          <h4 className="pokemon-name">{capitalize(pokemon.name)}</h4>
-          <img
-            src={pokemon.sprites.front_default}
-            alt="Pokemon"
-            height={144}
-            width={144}
-          />
-        </> :
-        <>
-          <h4 className="pokemon-name">?</h4>
-          <img
-            className="silhouette"
-            src={pokemon.sprites.front_default}
-            alt="Pokemon"
-            height={144}
-            width={144}
-          />
-        </>
-        }
-      </div>
-    </div>
+    <>
+      {pokemon.isCaught ?
+        <div className="flip-card" /*flip-card*/ >
+          <div className="pokemon-thumbnail-container" /*flip-card-inner*/ >
+            <div className="pokemon-thumbnail-front" /*flip-card-front*/ >
+              <h4 className="pokemon-name">{capitalize(pokemon.name)}</h4>
+              <img
+                id={pokemon.name}
+                className="pkmnImage-front-shadow silhouette"
+                src={pokemon.sprites.front_default}
+                alt="Pokemon"
+              />
+              <img
+                id={pokemon.name}
+                className="pkmnImage-front"
+                src={pokemon.sprites.front_default}
+                alt="Pokemon"
+              />
+
+            </div>
+            <div className={`pokemon-thumbnail-back ${pokemon.types[0].type.name}`} /*flip-card-back*/ >
+              <p className={`pokemon-id ${pokemon.types[0].type.name}`}>#{pokemon.id}</p>
+              <p className={`pokemon-name-back`}> {capitalize(pokemon.name)} </p>
+              <img
+                id={pokemon.name}
+                className="pkmnImage-back-shadow silhouette"
+                src={pokemon.sprites.front_default}
+                alt="Pokemon"
+              />
+              <img
+                id={pokemon.name}
+                className="pkmnImage-back"
+                src={pokemon.sprites.front_default}
+                alt="Pokemon"
+              />
+              <div className="pokemon-details">
+                <div className="type-container">
+                  {pokemon.types.length === 1 ? "Type:" : "Types:"}
+                  {pokemon.types.map((element) => {
+                    return <img src={`./assets/types/${element.type.name}-type.png`}
+                                id={element.type.name}
+                                className="pokemon-type"
+                            />
+                  })}
+                </div>
+                <div className="pokemon-detail-entry">Height: {Math.round(pokemon.height * 3.93701)} in.</div>
+                <div className="pokemon-detail-entry">Weight: {Math.round(pokemon.weight * 0.220462)} lbs</div>
+                <table className={`stat-table ${pokemon.types[0].type.name}`}>
+                  <th className={`stat-table-header ${pokemon.types[0].type.name}`}>Base Stats</th>
+                    {pokemon.stats.map(stat => {
+                      let statName = stat.stat.name;
+                      if (statName === "hp") {
+                        statName = "HP"
+                      }
+                      if (statName === "attack") {
+                        statName = "Atk"
+                      }if (statName === "defense") {
+                        statName = "Def"
+                      }
+                      if (statName === "speed") {
+                        statName = "Spd"
+                      }
+                      if (statName === "special-attack") {
+                        statName = "Sp-Atk"
+                      }
+                      if (statName === "special-defense") {
+                        statName = "Sp-Def"
+                      }
+                      return <tr className={`stat-table-row`}>
+                          <td className={`stat-table-cell ${pokemon.types[0].type.name}`}>{statName}</td>
+                          <td className={`stat-table-cell ${pokemon.types[0].type.name}`}>{stat.base_stat}</td>
+                        </tr>
+                    })}
+                </table>
+              </div>
+            </div>
+          </div>
+        </div> :
+        <div className="pokemon-thumbnail-hidden">
+          <div className="pokemon-thumbnail-silhouette">
+            <h4 className="pokemon-name">?</h4>
+            <img
+              id={pokemon.name}
+              className="pkmnImage silhouette"
+              src={pokemon.sprites.front_default}
+              alt="Pokemon"
+              height={144}
+              width={144}
+            />
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
